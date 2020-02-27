49e115489261940f4fd7e23a1238e07ef6818a69
Will Masek <willmasek@gmail.com>
2020-02-27 16:52:16 +0000

    Let's try this again.

--- Source/VehicleGame/Private/Pawns/VehicleMovementComponentBoosted4w.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
-
-#include "VehicleGame.h"
-#include "Pawns/VehicleMovementComponentBoosted4w.h"
-
-UVehicleMovementComponentBoosted4w::UVehicleMovementComponentBoosted4w(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
-{
-}
-
